3786eb367266c0c91f28ca96efd8b3fd44123038
Ryan Graham <r.m.graham@gmail.com>
2015-06-20 13:58:10 -0700

    test: add test for remote shell command

--- mktmpio/instance_test.go
+++ mktmpio/instance_test.go
@@ -35,3 +35,33 @@ func TestLoadEnv(t *testing.T) {
 		t.Error("pssword env var not 'pass':", val)
 	}
 }
+
+func TestCmdNoEnv(t *testing.T) {
+	instance := Instance{
+		ID:       "someId",
+		Host:     "some-host",
+		Port:     1234,
+		Type:     "mktmpdb",
+		Username: "user",
+		Password: "pass",
+		RemoteShell: shell{
+			Cmd: []interface{}{"tmpdbcli", "-h", "some-host", "-p", 1234},
+		},
+	}
+	cmd := instance.Cmd()
+	if cmd.Path != "tmpdbcli" {
+		t.Error("cmd.Path incorrect:", cmd.Path)
+	}
+	if cmd.Args[0] != cmd.Path {
+		t.Error("cmd.Path and cmd.Args[0] should be the same", cmd.Args[0])
+	}
+	if len(cmd.Args) != 5 {
+		t.Error("cmd.Args wrong length:", len(cmd.Args))
+	}
+	if cmd.Args[4] != "1234" {
+		t.Error("int argument was not stringified correctly:", cmd.Args[4])
+	}
+	if len(cmd.Env) != 0 {
+		t.Error("no env variables should be set", cmd.Env)
+	}
+}
