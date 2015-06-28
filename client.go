106f05ee77b1fab5e30de0d73a114359e42f9326
Ryan Graham <r.m.graham@gmail.com>
2015-06-27 21:33:05 -0700

    remove commented out code

--- client.go
+++ client.go
@@ -91,20 +91,4 @@ func (c Client) Attach(id string) (io.Reader, io.Writer, error) {
 	cfg.Header.Set("X-Auth-Token", c.token)
 	ws, err := websocket.DialConfig(cfg)
 	return ws, ws, err
-	// if err != nil {
-	// 	return nil, nil, err
-	// 	//log.Fatal(err)
-	// }
-	// if _, err := ws.Write([]byte("hello, world!\n")); err != nil {
-	// 	return nil, nil, err
-	// 	// log.Fatal(err)
-	// }
-	// var msg = make([]byte, 512)
-	// var n int
-	// if n, err = ws.Read(msg); err != nil {
-	// 	return nil, nil, err
-	// 	// log.Fatal(err)
-	// }
-	// fmt.Printf("Received: %s.\n", msg[:n])
-	// return ws, ws, error
 }
