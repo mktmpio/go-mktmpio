e8cab91f4a5fdae0ae014e39a4b78c2da8cfcc11
Ryan Graham <r.m.graham@gmail.com>
2015-11-15 20:50:45 -0800

    metadata: add license and hide docs

---
+++ meta.go
@@ -0,0 +1,12 @@
+// Copyright 2015 Datajin Technologies, Inc. All rights reserved.
+// Use of this source code is governed by an Artistic-2
+// license that can be found in the LICENSE file.
+
+package mktmpio
+
+// exposed for package introspection and
+const (
+	Copyright   = "Copyright 2015 Datajin Technologies, Inc. All rights reserved."
+	LicenseName = "Artistic-2.0"
+	LicenseURL  = "http://opensource.org/licenses/Artistic-2.0"
+)
